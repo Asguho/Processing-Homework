9911a07448e5574e8e85dcc07184cd865a934a1e
Asguho <78410939+Asguho@users.noreply.github.com>
2024-04-02 18:52:05 +0200

    added javatest

---
+++ Asguho/javatest/spil/src/main/java/com/javatest/Main.java
@@ -0,0 +1,9 @@
+package com.javatest;
+
+import processing.core.*;;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!" + PConstants.PI);
+    }
+}
