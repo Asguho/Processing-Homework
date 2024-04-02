6af54dfc0bedef64f6fad85859821532690fa4bb
Asguho <78410939+Asguho@users.noreply.github.com>
2024-04-02 19:08:15 +0200

    Update Main.java

--- Asguho/javatest/spil/src/main/java/com/javatest/Main.java
+++ Asguho/javatest/spil/src/main/java/com/javatest/Main.java
@@ -1,9 +1,44 @@
 package com.javatest;
 
-import processing.core.*;;
+import processing.core.*;
 
-public class Main {
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main extends processing.core.PApplet {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!" + PConstants.PI);
+        PApplet.main("com.javatest.Main");
+    }
+
+    public void settings() {
+        size(800, 600);
+    }
+
+    public void setup() {
+        background(0);
+        String joke;
+        try {
+            joke = HttpClient.newHttpClient().send(
+                    HttpRequest.newBuilder(URI.create("https://api.chucknorris.io/jokes/random")).build(),
+                    HttpResponse.BodyHandlers.ofString()).body();
+        } catch (IOException e) {
+            joke = "IOException";
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            joke = "IOException";
+            e.printStackTrace();
+        }
+        println(joke);
+        text(joke, width / 2, height / 2);
+    }
+
+    public void draw() {
+        fill(255);
+        noStroke();
+        ellipse(mouseX, mouseY, 80, 80);
     }
 }
